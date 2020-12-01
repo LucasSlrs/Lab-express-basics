cd3a13da805c66ef75a2390d4f73b6f2e32e8def
Lucas Souleyrasse <lucas.souleyrasse@gmail.com>
2020-12-01 19:07:39 +0100

    css changed

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
-const btn = document.getElementById("whoAreWe");
 const sec = document.querySelector('.fisrt-section');
+document.getElementById("whoAreWe").textContent = `<p class="description">Welcome to our Website, this is a IronHack lab to let us
+work with Express.js as an introduction to the backend development</p>`
 
-const handleToggle = () => sec.classList.toggle("is-active");
+const handleToggle = () => sec.classList.toggle("is-active", text);
 if (btn !== null) btn.onclick = handleToggle;
