fe9b9c31df9f860de0c8403efc1e597170406336
Lucas Souleyrasse <lucas.souleyrasse@gmail.com>
2020-12-02 12:32:42 +0100

    lab not finish, but main content is here...

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,19 @@
 const target = document.querySelector('.first-section-btn');
 const btn = document.getElementById('whoAreWe');
-const createDiv = `<div class="description">Welcome to our Website, this Yis a IronHack lab to let us
+// const createDiv = `<div class="description">Welcome to our Website, this Yis a IronHack lab to let us
 // work with Express.js as an introduction to the backend development</div>`;
-
-const hangleToggle = () => btn.classList.toggle('is-visible', createDiv);
+// const createDiv = document.createElement('div');
+// createDiv.className = "description";
+// createDiv.textContent = `Welcome to our Website, this Yis a IronHack lab to let us
+// work with Express.js as an introduction to the backend development`;
+//rajouter un appenchild
+const createDiv = document.querySelector(".description");
+const hangleToggle = () => createDiv.classList.toggle('is-visible');
 if (btn !== null) btn.onclick = hangleToggle;
+console.log(btn);
+console.log(createDiv);
+// const hangleToggle = () => createDiv.classList.toggle('is-visible');
+
 
 
 
